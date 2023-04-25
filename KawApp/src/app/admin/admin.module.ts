2cdf6ccff27f0213304ee32623ad78b03ab4466d
thomasboidun <thomas.boidun@gmail.com>
2023-04-25 16:11:20 +0200

    add admin module

---
+++ KawApp/src/app/admin/admin.module.ts
@@ -0,0 +1,17 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { SharedModule } from '../shared/shared.module';
+import { AdminRoutingModule } from './admin-routing.module';
+
+@NgModule({
+    declarations: [
+        
+    ],
+    imports: [
+        CommonModule,
+        SharedModule,
+        AdminRoutingModule
+    ],
+    exports: [],
+})
+export class AdminModule { }
