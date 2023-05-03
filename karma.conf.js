ed42edb756baecc1f0ada302a1000841cd29ebf9
benjamin <benjamin.emmanuel@epsi.fr>
2023-05-03 18:41:07 +0200

    add job build to gitlab ci yml

--- karma.conf.js
+++ karma.conf.js
@@ -15,14 +15,14 @@ module.exports = function (config) {
       jasmineHtmlReporter: {
         suppressAll: true // removes the duplicated traces
       },
-      //coverageReporter: {
-        //dir: require('path').join(__dirname, ''),
-        //subdir: '.',
-        //reporters: [
-          //{ type: 'html' },
-          //{ type: 'text-summary' }
-        //]
-      //},
+      coverageReporter: {
+        dir: require('path').join(__dirname, ''),
+        subdir: '.',
+        reporters: [
+          { type: 'html' },
+          { type: 'text-summary' }
+        ]
+      },
       reporters: ['progress', 'kjhtml'],
       port: 9876,
       colors: true,
