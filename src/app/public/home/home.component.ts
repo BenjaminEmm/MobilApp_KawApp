e90961a8c695bc649f7e5ffaf6e989b659d3021d
thomasboidun <thomas.boidun@gmail.com>
2023-05-04 23:30:11 +0200

    refactoring

--- src/app/public/home/home.component.ts
+++ src/app/public/home/home.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { CustomerService } from 'src/app/shared/services/customer.service';
-import { OrderService } from 'src/app/shared/services/order.service';
 
 @Component({
   selector: 'app-home',
@@ -8,36 +6,9 @@ import { OrderService } from 'src/app/shared/services/order.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  constructor(
-    private customerService: CustomerService,
-    private orderService: OrderService,
-  ) { }
 
-  ngOnInit() {
-    // this.customerService.getById(7).subscribe(
-    //   response => {
-    //     console.log(response);
-    //   }, error => { console.error(error); }
-    // )
+  constructor() { }
 
-    // this.customerService.getAll().subscribe(
-    //   response => {
-    //     console.log(response);
-    //   }, error => { console.error(error); }
-    // )
-
-    // this.orderService.getAllByCustomerId(7).subscribe(
-    //   response => {
-    //     console.log(response);
-    //   }, error => { console.error(error); }
-    // )
-
-    this.orderService.getById(7, 7).subscribe(
-      response => {
-        console.log(response);
-      }, error => { console.error(error); }
-    )
-
-
-  }
+  ngOnInit() { }
+  
 }
