2159fdc84943861eec10a3fb504dfe5677a76e46
benjamin <benjamin.emmanuel@epsi.fr>
2023-05-04 20:03:22 +0200

    changes karma.conf

--- karma.conf.js
+++ karma.conf.js
@@ -28,13 +28,13 @@ module.exports = function (config) {
       colors: true,
       logLevel: config.LOG_INFO,
       autoWatch: true,
-      browsers: ['Chrome', "ChromeHeadless"],
+      browsers: ['Chrome', 'ChromeHeadless','ChromeHeadlessCI'],
       singleRun: false,
       restartOnFileChange: true,
       customLaunchers: {
-        ChromeHeadless: {
+        ChromeHeadlessCI: {
           base: 'ChromeHeadless',
-          flags: ['--no-sandbox']
+          flags: ['--no-sandbox','--disable-gpu']
         }
       }
     });
