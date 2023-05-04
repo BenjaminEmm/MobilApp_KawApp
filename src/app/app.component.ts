e7a14866ef2d83cca467152ee10e366cae981807
thomasboidun <thomas.boidun@gmail.com>
2023-05-05 00:10:26 +0200

    added a function that allows to use the application as a user or administrator (dev env).

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { CurrentUserService } from './core/auth/current-user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'KawApp';
+
+  constructor(
+    private currentUserService: CurrentUserService,
+  ) { }
+
+  ngOnInit(): void {
+    if (!environment.production) this.useAppAsA('user');
+  }
+
+  /**
+   * Allows you to use the application as a default user or default admin.
+   * @param {string} role The user role. Can be `'user'` or `'admin'`.
+   */
+  private useAppAsA(role: string): void {
+    if (!environment.production) {
+      switch (role) {
+        case 'admin':
+          this.currentUserService.save(environment.default_admin.token);
+          break;
+        case 'user':
+          this.currentUserService.save(environment.default_user.token);
+          break;
+        default:
+          this.currentUserService.save(environment.default_user.token);
+          break;
+      }
+      console.info('You use the application with a default user:', this.currentUserService.currentUser);
+    }
+    else {
+      console.error(`Impossible to use the application with a default ${role}!`);
+    }
+  }
 }
