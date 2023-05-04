c47683dc551d4bb1586be3e6e81506c36dfa4678
thomasboidun <thomas.boidun@gmail.com>
2023-05-04 23:46:33 +0200

    add default tokens in dev env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,20 +3,28 @@
 // The list of file replacements can be found in `angular.json`.
 const production = false;
 
-const uri = 'https://615f5fb4f7254d0017068109.mockapi.io/api/v1';
+const uri = '';
 
-const token = 'ADMIN_TOKEN';
+const default_admin_token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImRlZmF1bHRfYWRtaW4iLCJpZCI6MCwicm9sZSI6ImFkbWluIiwiaWF0IjoxNTE2MjM5MDIyfQ.xAtDHzG5aXvd_XpsFmpuTRPdrG6_Sc6AYTTkqpsbvLM';
+const default_user_token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImRlZmF1bHRfdXNlciIsImlkIjowLCJyb2xlIjoidXNlciIsImlhdCI6MTUxNjIzOTAyMn0.LND5ZJTsXAqY7WJjfhLewk5mbtzq43IoeaMgtZF0ZbI';
 
-const admin = {
-  token,
+const default_admin = {
+  token: default_admin_token,
   requestOptions: {
     headers: {
-      'authorization': `Bearer ${token}`
+      'authorization': `Bearer ${default_admin_token}`
     }
   }
 };
 
-export const environment = { production, uri, admin };
+const default_user = {
+  token: default_user_token,
+  requestOptions: {
+    headers: { 'authorization': `Bearer ${default_user_token}` }
+  }
+}
+
+export const environment = { production, uri, default_admin, default_user };
 
 /*
  * For easier debugging in development mode, you can import the following file
