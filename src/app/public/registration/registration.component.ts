e7fd394557b66418c9818fed57acf95aaefa2096
thomasboidun <thomas.boidun@gmail.com>
2023-05-10 11:38:18 +0200

    registration intergration

--- src/app/public/registration/registration.component.ts
+++ src/app/public/registration/registration.component.ts
@@ -1,36 +1,51 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthenticationService } from 'src/app/core/auth/authentication.service';
 import { RegistrationData } from 'src/app/shared/classes/RegistrationData';
+import { CustomerService } from 'src/app/shared/services/customer.service';
 
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
   styleUrls: ['./registration.component.css']
 })
-export class RegistrationComponent implements OnInit{
+export class RegistrationComponent implements OnInit {
 
   public registrationForm = new FormGroup({
-    username: new FormControl(null, [Validators.required]),
-    firstName: new FormControl(null, [Validators.required]),
-    lastName: new FormControl(null, [Validators.required]),
-    city: new FormControl(null, [Validators.required]),
-    zipcode: new FormControl(null, [Validators.required]),
-    company: new FormControl(null, [Validators.required]),
+    adresseMail: new FormControl(null, [Validators.required, Validators.email]),
+    password: new FormControl(null, [Validators.required]),
   });
 
+  public seconds: number = 5;
+  public success!: boolean;
+  public error!: boolean;
+
   constructor(
-    // private userService: UserService,
+    private authService: AuthenticationService,
+    private router: Router,
   ) { }
 
   ngOnInit(): void {
-      
+
   }
 
   onSubmit(): void {
-    console.log('onSubmit');
     const args = this.registrationForm.value;
     const registrationData = new RegistrationData(args);
-    console.log(registrationData)
+    this.authService.signUp(registrationData).subscribe(
+      (res: any) => {
+        this.success = true;
+        const interval = setInterval(() => {
+          this.seconds--;
+          if (this.seconds === 0) {
+            this.router.navigate(['authentication']).then(() => {
+              this.seconds = 5;
+              clearInterval(interval);
+            });
+          }
+        }, 1000);
+      }
+    )
   }
-
 }
--- src/app/shared/classes/RegistrationData.ts
+++ src/app/shared/classes/RegistrationData.ts
@@ -1,18 +1,9 @@
 export class RegistrationData {
-    public username: string;
-    public firstName: string;
-    public lastName: string;
-    public city: string;
-    public zipcode: string;
-    public company: string;
+    public adresseMail!: string;
+    public password!: string;
 
     constructor(args: any = null) {
-        this.username = args?.username;
-        this.firstName = args?.firstName;
-        this.lastName = args?.lastName;
-        this.city = args?.city;
-        this.zipcode = args?.zipcode;
-        this.company = args?.company;
+        this.adresseMail = args?.adresseMail;
+        this.password = args?.password;
     }
-
 }
